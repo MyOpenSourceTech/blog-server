22f6f4e2895325f5e71ff78f5789c008b52393b9
Sourav Kabiraj <sourav.kabiraj@outlook.com>
2020-07-19 16:02:32 +0530

    #BS-007 | Sourav | Enabled eureka client.

--- src/main/java/com/coffeebrew/blogserver/BlogServerApplication.java
+++ src/main/java/com/coffeebrew/blogserver/BlogServerApplication.java
@@ -2,8 +2,10 @@ package com.coffeebrew.blogserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BlogServerApplication {
 
 	public static void main(String[] args) {
