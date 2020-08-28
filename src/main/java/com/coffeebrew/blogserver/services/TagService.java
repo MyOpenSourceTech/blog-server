f7017fb6e223cc65ffca198453ed5f5cb5d08a59
Sourav Kabiraj <sourav.kabiraj@outlook.com>
2020-08-29 00:36:16 +0530

    #BS-009 | Sourav | Implemented search by label feature for Tags.

--- src/main/java/com/coffeebrew/blogserver/services/TagService.java
+++ src/main/java/com/coffeebrew/blogserver/services/TagService.java
@@ -3,7 +3,9 @@ package com.coffeebrew.blogserver.services;
 import com.coffeebrew.blogserver.models.Tag;
 import com.coffeebrew.blogserver.repositories.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
@@ -19,5 +21,9 @@ public class TagService {
 
     public Tag create(Tag tag) {
         return tagRepository.save(tag);
+    }
+
+    public Optional<Tag> getByLabel(String label) {
+        return tagRepository.findByLabel(label);
     }
 }
